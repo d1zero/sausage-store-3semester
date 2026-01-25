ed5a7692639001104e2b0d345a7baab5500c9954
Alex <asashek@yandex.ru>
2026-01-25 07:27:20 +0300

    vars fix

--- infra/variable.tf
+++ infra/variable.tf
@@ -5,3 +5,14 @@ variable "cloud_id" {
 variable "folder_id" {
   type = string
 }
+
+variable "ssh_public_key" {
+  type = string
+  description = "SSH public key"
+}
+
+variable "ssh_private_key" {
+  type = string
+  description = "SSH private key"
+  sensitive = true
+}
